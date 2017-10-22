76478b5b5169e799c91afb443f1107dd65ece2ad
Madison Scott-Clary <madison.scott-clary@canonical.com>
2017-10-21 23:38:34 -0600

    checkpoint

--- logging/logging.go
+++ logging/logging.go
@@ -11,6 +11,14 @@ import (
 
 var logger *zap.SugaredLogger
 
+func LevelFromString(levelString string) (zapcore.Level, error) {
+	var level zapcore.Level
+	if err := level.UnmarshalText([]byte(levelString)); err != nil {
+		return level, err
+	}
+	return level, nil
+}
+
 // Setup builds a sugared logger for use throughout the application.
 func Setup(level zapcore.Level) error {
 	cfg := zap.NewProductionConfig()
